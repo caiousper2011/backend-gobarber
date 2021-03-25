8a345969297c695314bb6b4582c0369837a72ba7
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2021-03-24 21:22:58 -0300

    dfg

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -4,6 +4,8 @@ import AppointmentController from '../controllers/AppointmentController';
 const appointmentsRouter = Router();
 const { create } = new AppointmentController();
 
+console.log('teste final husky');
+
 // appointmentsRouter.get('/', async (request, response) => {
 //   const appointments = await appointmentsRepository.find();
 //   return response.json(appointments);
