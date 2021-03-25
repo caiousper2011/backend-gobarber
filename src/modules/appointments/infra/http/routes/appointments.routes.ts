18c790d0ab4ca5810bde5d4f7fb8062b4c83a7bd
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2021-03-24 21:23:38 -0300

    finalização da configuração do pre-commit

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -4,8 +4,6 @@ import AppointmentController from '../controllers/AppointmentController';
 const appointmentsRouter = Router();
 const { create } = new AppointmentController();
 
-console.log('teste final husky');
-
 // appointmentsRouter.get('/', async (request, response) => {
 //   const appointments = await appointmentsRepository.find();
 //   return response.json(appointments);
